60ab483d5fb0e807d9c5e23c5e7c2809ccb35a63
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-02-22 06:14:07 -0800

    2/22/21

--- week2/blog-props/src/Components/Footer.js
+++ week2/blog-props/src/Components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Footer(){
-  let footer = "Copyright © Your Website 2009";
+  let footer = "Copyright © Your Website 2021";
   return <div id="footer">{footer}</div>;
 }
 export default Footer;

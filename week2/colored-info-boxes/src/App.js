7fbefdcf1cd58c7c22edea635da3398aa50bb039
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-02-22 02:54:33 -0800

    2/22/21

--- week2/colored-info-boxes/src/App.js
+++ week2/colored-info-boxes/src/App.js
@@ -4,62 +4,62 @@ function App() {
   return (
     <div>
       <InfoBox
-        title="Hello World"
+        title="Hello World!!!!"
         subTitle="I am Dean Wilcoxson"
         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a fringilla turpis, a tempor nibh. Ut faucibus, ex eget volutpat placerat, leo velit suscipit enim, id 
          tellus turpis non risus."
         bgColor="blue"
       />
       <InfoBox
-        title="Hello World"
+        title="Hello World!!!!"
         subTitle="I am Dean Wilcoxson"
         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a fringilla turpis, a tempor nibh. Ut faucibus, ex eget volutpat placerat, leo velit suscipit enim, id porttitor tellus turpis non risus."
         bgColor="green"
       />
       <InfoBox
-        title="Hello World"
+        title="Hello World!!!!"
         subTitle="I am Dean Wilcoxson"
         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a fringilla turpis, a tempor nibh. Ut faucibus, ex eget volutpat placerat, leo velit suscipit enim, id porttitor tellus turpis non risus."
         bgColor="yellow"
       />
       <InfoBox
-        title="Hello World"
+        title="Hello World!!!!"
         subTitle="I am Dean Wilcoxson"
         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a fringilla turpis, a tempor nibh. Ut faucibus, ex eget volutpat placerat, leo velit suscipit enim, id porttitor tellus turpis non risus."
         bgColor="orange"
       />
       <InfoBox
-        title="Hello World"
+        title="Hello World!!!!"
         subTitle="I am Dean Wilcoxson"
         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a fringilla turpis, a tempor nibh. Ut faucibus, ex eget volutpat placerat, leo velit suscipit enim, id porttitor tellus turpis non risus."
         bgColor="red"
       />
       <InfoBox
-        title="Hello World"
+        title="Hello World!!!!"
         subTitle="I am Dean Wilcoxson"
         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a fringilla turpis, a tempor nibh. Ut faucibus, ex eget volutpat placerat, leo velit suscipit enim, id porttitor tellus turpis non risus."
         bgColor="purple"
       />
       <InfoBox
-        title="Hello World"
+        title="Hello World!!!!"
         subTitle="I am Dean Wilcoxson"
         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a fringilla turpis, a tempor nibh. Ut faucibus, ex eget volutpat placerat, leo velit suscipit enim, id porttitor tellus turpis non risus."
         bgColor="magenta"
       />
       <InfoBox
-        title="Hello World"
+        title="Hello World!!!!"
         subTitle="I am Dean Wilcoxson"
         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a fringilla turpis, a tempor nibh. Ut faucibus, ex eget volutpat placerat, leo velit suscipit enim, id porttitor tellus turpis non risus."
         bgColor="pink"
       />
       <InfoBox
-        title="Hello World"
+        title="Hello World!!!!"
         subTitle="I am Dean Wilcoxson"
         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a fringilla turpis, a tempor nibh. Ut faucibus, ex eget volutpat placerat, leo velit suscipit enim, id porttitor tellus turpis non risus."
         bgColor="lightgrey"
       />
       <InfoBox
-        title="Hello World"
+        title="Hello World!!!!"
         subTitle="I am Dean Wilcoxson"
         info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a fringilla turpis, a tempor nibh. Ut faucibus, ex eget volutpat placerat, leo velit suscipit enim, id porttitor tellus turpis non risus."
         bgColor="skyblue"
--- week2/colored-info-boxes/src/InfoBox.js
+++ week2/colored-info-boxes/src/InfoBox.js
@@ -1,20 +1,31 @@
-import { React} from "react";
-function InfoBox(props){  
-    let info = props.info;
-    let title = props.title;
-    let bgColor = props.bgColor;
-    let subTitle = props.subTitle;
-    let style = {
-        padding: "20px",
-        marginTop: "5px",
-    }
-    return (
-        <div style={style, {backgroundColor:bgColor, color:"black"}}>
-            <h1>Title: {title}</h1>
-            <h3 style={{paddingLeft:"20px"}}>Subtitle: {subTitle}</h3>
-            <p style={{paddingLeft:"20px"}}>Info: {info}</p>
-            <p style={{paddingLeft:"20px"}}>Background Color: {bgColor}</p>
-        </div>
-    )
-  }
+import { React } from "react";
+function InfoBox(props) {
+  let info = props.info;
+  let title = props.title;
+  let bgColor = props.bgColor;
+  let subTitle = props.subTitle;
+  let divStyle = {
+    backgroundColor: bgColor,
+    color: "black",
+    borderRadius: "15px",
+    boxShadow: "2px 2px 4px grey",
+  };
+  let h1Style = {
+    marginTop: "15px",
+    paddingTop: "15px",
+    textAlign: "center",
+    textShadow:"3px 3px 5px black",
+    textDecoration:"underline",
+  };
+  let contentStyle = { paddingLeft: "20px", textAlign:"center" };
+  return (
+    <div style={divStyle}>
+      <h1 style={h1Style}>{title}</h1>
+      <hr />
+      <h3 style={contentStyle}>{subTitle}</h3>
+      <p style={contentStyle}>{info}</p>
+      <p style={contentStyle}>Background Color: {bgColor}</p>
+    </div>
+  );
+}
 export default InfoBox;

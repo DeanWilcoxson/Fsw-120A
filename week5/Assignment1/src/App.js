8fcfc57f82172a8249a67ccdaddde576628e7a88
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-03-18 20:30:34 -0700

    3/18/21

---
+++ week5/Assignment1/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+function App() {
+  let styles = { textAlign:"center", }
+  return (
+    <div style={styles}>
+    <h1>Dean's Emergency Piping and Plumbing</h1>
+      <Navbar /> 
+      
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/About">
+          <About />
+        </Route>
+        <Route exact path="/Contact">
+          <Contact />
+        </Route>
+        <Route exact path="/Services">
+          <Services />
+        </Route>
+      </Switch>
+      
+      <Footer />
+
+    </div>
+  );
+}
+export default App;
---
+++ week5/Assignment1/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Footer(){
+    let styles = {
+        textAlign: "center",
+    }
+    return(
+        <div style={styles}>
+            <footer>Super Awesome footer info</footer>
+        </div>
+    )
+}
+export default Footer;
